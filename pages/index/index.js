813359d5df44eb1f579a2df46341d2938141910e
转身打铁 <2300649893@qq.com>
2020-10-20 15:46:21 +0800

    完成首页开发

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,43 @@
 //Page Object
+import { request } from "../../request/index.js"
 Page({
   data: {
-
+    swiperList: [],
+    cateList: [],
+    floorList: []
   },
   //options(Object)
   onLoad: function (options) {
-
+    this.getSwiperList();
+    this.getCateList();
+    this.getFloorList()
+  },
+  // 获取轮播图数据
+  getSwiperList() {
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata" })
+      .then(result => {
+        this.setData({
+          swiperList: result.data.message
+        })
+      })
+  },
+  // 获取分类导航数据
+  getCateList() {
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/home/catitems" })
+      .then(result => {
+        this.setData({
+          cateList: result.data.message
+        })
+      })
+  },
+  // 获取楼层数据
+  getFloorList() {
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/home/floordata" })
+      .then(result => {
+        this.setData({
+          floorList: result.data.message
+        })
+      })
   },
   onReady: function () {
 
