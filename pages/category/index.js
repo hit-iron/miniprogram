a458d002986d750e8d910c20670365756cd1be14
转身打铁 <2300649893@qq.com>
2020-10-21 16:55:15 +0800

    分类页面完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,10 +1,44 @@
-// pages/category/index.js
+import { request } from "../../request/index.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    leftMunuList: [],
+    rightContent: [],
+    // 被选中的当前索引
+    currentIndex: 0,
+    scrollTop: 0
+
+  },
+  // 分类接口返回的所有数据
+  Cates: [],
+  // 获取分类数据
+  getCategoryList() {
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/categories" })
+      .then(res => {
+        this.Cates = res.data.message;
+        // 把接口的数据存入到本地存储中
+        wx.setStorageSync("cates", { time: Date.now(), data: this.Cates });
+        let leftMenuList = this.Cates.map(v => v.cat_name);
+        let rightContent = this.Cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+
+      })
+  },
+  handleItemTap(e) {
+    const { index } = e.currentTarget.dataset;
+    let rightContent = this.Cates[index].children;
+    this.setData({
+      currentIndex: index,
+      // 右边数据
+      rightContent,
+      scrollTop: 0
+    });
 
   },
 
@@ -12,7 +46,29 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //  1 获取本地存储中的数据  (小程序中也是存在本地存储 技术)
+    const Cates = wx.getStorageSync("cates");
+    // 2 判断
+    if (!Cates) {
+      // 不存在  发送请求获取数据
+      this.getCategoryList();
+    } else {
+      // 有旧的数据 定义过期时间  10s 改成 5分钟
+      if (Date.now() - Cates.time > 1000 * 10) {
+        // 重新发送请求
+        this.getCategoryList();
+      } else {
+        // 可以使用旧的数据
+        this.Cates = Cates.data;
+        let leftMenuList = this.Cates.map(v => v.cat_name);
+        let rightContent = this.Cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightContent,
 
+        })
+      }
+    }
   },
 
   /**
