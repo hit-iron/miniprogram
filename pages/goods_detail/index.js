74e6bce0878ca2e41c4ae59fd74b1ee0be044ec9
转身打铁 <2300649893@qq.com>
2020-10-29 19:12:48 +0800

    完成了商品详情页面

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,17 +1,64 @@
-// pages/goods_detail/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsObj: {}
   },
+  GoodsInfo: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let { goods_id } = options;
+
+    this.getGoodsDetail(goods_id);
+  },
+  async getGoodsDetail(goods_id) {
+    const goodsObj = await request({ url: "/goods/detail", data: { goods_id } });
+    this.GoodsInfo = goodsObj;
+    this.setData({
+      goodsObj: {
+        goods_name: goodsObj.goods_name,
+        goods_price: goodsObj.goods_price,
+        goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: goodsObj.pics
+      }
+    })
+  },
+  handlePreviewImage(e) {
+    const urls = this.GoodsInfo.pics.map(v => v.pics_mid);
+    const current = e.currentTarget.dataset.url;
+    wx.previewImage({
+
+      current,
+      urls
+    });
+
+  },
+  // 点击 加入购物车
+  handleCartAdd() {
+    let cart = wx.getStorageSync("cart") || [];
+    let index = cart.findIndex(v => v.goods_id === this.GoodsInfo.goods_id);
+    if (index === -1) {
+      this.GoodsInfo.num = 1;
+      this.GoodsInfo.checked = true;
+      cart.push(this.GoodsInfo);
+    } else {
+      cart[index].num++;
+    }
+    wx.setStorageSync("cart", cart);
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      mask: true
+    });
+
+
 
   },
 
